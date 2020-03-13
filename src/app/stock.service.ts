611e21bb0f280ec89e88934a0f3ef1a97244a657
amantes <amantes@intercardinc.com>
2020-03-13 13:51:31 +0800

    changed baseURL for dev and prod

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -2,20 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Stocks } from './models/stock';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
+  baseUrl = environment.baseUrl;
 
   constructor( private http:HttpClient) { }
 
   getStocks():Observable<Stocks>{
-    return this.http.get<Stocks>('./assets/fetchdata.php')
+    return this.http.get<Stocks>(this.baseUrl + 'fetchdata.php')
   }
 
   deleteStock(symbol){
     let inData = {"symbol": symbol};
-    this.http.post('./assets/deletestock.php', inData);
+    this.http.post( this.baseUrl + 'deletestock.php', inData);
   }
 }
